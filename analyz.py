aea06c4bb0f9fb70c872a73be0a30d6f5f3e5e38
utkudarilmaz <utkudarilmaz@gmail.com>
2018-12-18 00:49:14 +0300

    Added some exception state for breaks

--- analyz.py
+++ analyz.py
@@ -5,7 +5,7 @@ import time
 import datetime
 
 try:
-    client = InfluxDBClient(host='172.17.0.3', port=8086, username='test', password='123456')
+    client = InfluxDBClient(host='192.168.1.240', port=8086, username='test', password='123456')
     client.switch_database('test')
 except e:
     print(e)
@@ -21,19 +21,19 @@ except FileNotFoundError:
         print(k)
         exit()
 
-# try:
-#     os.chdir("/var/log/tshark")
-#     capture = "tshark -O icmp -T json -w log.cap -b duration:30 &"
-#     os.system(capture)
-# except e:
-#     print(e)
-#     exit()
+try:
+    os.chdir("/var/log/tshark")
+    capture = "tshark -O icmp -T json -w log.cap -b duration:30 &"
+    os.system(capture)
+except e:
+    print(e)
+    exit()
 
 host = {}
 
 while True:
 
-    files = os.listdir()
+    files = os.listdir(path='/var/log/tshark/')
     files.sort()
 
     if len(files) > 2:
@@ -42,32 +42,38 @@ while True:
         a = rdpcap(file)
 
         for i in range(len(a)):
+            try:
+                if a[i].type == 2048:
 
-            if a[i].type == 2048:
+                    source = a[i][IP].src
+                    target = a[i][IP].dst
 
-                source = a[i][IP].src
-                target = a[i][IP].dst
+                    header = "logs"
 
-                header = "logs"
-
-                try:
-                    if a[i][ICMP].type == 8:
-                         log = [{
-                             'measurement': header,
-                             'tags': {
-                                 'source': source,
-                                 'target': target,
-                             },
-                             'time': datetime.datetime.utcnow(),
-                             "fields": {
-                                 "value": float(1),
-                             },
-                         }]
-                         client.write_points(log)
+                    try:
+                        if a[i][ICMP].type == 8:
+                             log = [{
+                                 'measurement': header,
+                                 'tags': {
+                                     'source': source,
+                                     'target': target,
+                                 },
+                                 'time': datetime.datetime.utcnow(),
+                                 "fields": {
+                                     "value": float(1),
+                                 },
+                             }]
+                             client.write_points(log)
+                        else:
+                            continue
+                    except IndexError:
+                        continue
                     else:
                         continue
-                except IndexError:
+                else:
                     continue
+            except AttributeError as e:
+                continue
             else:
                 continue
         os.remove(file)
